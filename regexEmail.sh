e14e11fa68f15a7386856a91a281fd49751ef9ae
himanshugharat <himanshugharat44@gmail.com>
2020-07-17 23:34:27 -0500

    ADDED:noe we validate the first name of user

--- regexEmail.sh
+++ regexEmail.sh
@@ -1,1 +1,12 @@
 #!/bin/bash -x
+echo "Enter The FirstName:"
+read FNAME;
+PAT="^[A-Z]{1}[a-z]{2,}$";
+if [[ $FNAME =~ $PAT ]]
+then
+	echo "FirstName Is Valid";
+else
+	echo "FirstName Is Not Valid";
+fi
+
+
