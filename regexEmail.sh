55950f0771bc30e270c76d47e99e86f531547d2c
himanshugharat <himanshugharat44@gmail.com>
2020-07-17 23:35:42 -0500

    ADDED:now we validate the last name of user

--- regexEmail.sh
+++ regexEmail.sh
@@ -9,4 +9,13 @@ else
 	echo "FirstName Is Not Valid";
 fi
 
+echo "Enter The LastName:"
+read LNAME;
+PAT1="^[A-Z]{1}[a-z]{2,}$";
+if [[ $LNAME =~ $PAT1 ]]
+then
+	echo "LastName Is Valid";
+else
+	echo "LastName Is Not Valid";
+fi
 
